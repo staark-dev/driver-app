ac8b0ea928394cb1d6b34defda1112b3973af120
Costin Ionuț <51002637+staark-dev@users.noreply.github.com>
2025-08-13 20:30:58 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'truck-app-v12'; // bump când publici
+const CACHE = 'truck-app-v16'; // bump când publici
 const CORE  = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
